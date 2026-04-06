352eb768bdedc124afe731104d5c4378f3217831
Veries Seals <veries@me.com>
2026-04-05 21:10:15 -0500

    created class for NaturalizationRecord, updated python .venv environment

--- naturalization/models.py
+++ naturalization/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
+from persons.models import Person
 
 # Create your models here.
+# ------------------------------------------
+
+class NaturalizationRecord(models.Model):
+    pass
