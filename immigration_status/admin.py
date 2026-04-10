297379175069be4735f4827b58d083efff884c43
Veries Seals <veries@me.com>
2026-04-10 13:56:03 -0500

    Added logic for immigration_status for admin.py

--- immigration_status/admin.py
+++ immigration_status/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ImmigrationStatus
 
 # Register your models here.
+# --------------------------------------------
+
+# Register the ImmigrationStatus model with the admin site.
+# ----------------------------------------
+admin.site.register(ImmigrationStatus)
