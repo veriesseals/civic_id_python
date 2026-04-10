1fd8f8a30a832d05dd85c1fae347cd9a987dcbf1
Veries Seals <veries@me.com>
2026-04-10 18:02:14 -0500

    Added logic for Audit admin.py

--- audit/admin.py
+++ audit/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import AuditLog
 
 # Register your models here.
+# --------------------------------------------
+
+# Register the AuditLog model with the admin site.
+# ----------------------------------------
+admin.site.register(AuditLog)
