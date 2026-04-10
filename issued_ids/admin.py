6e37f7b8f63e43587838892d48cd6dcb850e291c
Veries Seals <veries@me.com>
2026-04-10 14:03:43 -0500

    Added logic for issued_id admin.py

--- issued_ids/admin.py
+++ issued_ids/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import IssuedID
 
 # Register your models here.
+# --------------------------------------------
+
+# Register the IssuedID model with the admin site.
+# ----------------------------------------
+admin.site.register(IssuedID)
