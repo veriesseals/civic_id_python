eddf839c95a7bd9c18cf8aeb6de364c80c850971
Veries Seals <veries@me.com>
2026-04-10 12:37:25 -0500

    Added logic to register the Naturalization Record model with the admin site

--- birth_records/admin.py
+++ birth_records/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import NaturalizationRecord
 
 # Register your models here.
+# ------------------------------------------
+
+# Register the NaturalizationRecord model with the admin site.
+# ----------------------------------------
+admin.site.register(NaturalizationRecord)
