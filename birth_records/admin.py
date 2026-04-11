ee12a3bee0dea4d03461b97bb67b4cd144740e0b
Veries Seals <veries@me.com>
2026-04-11 11:20:35 -0500

    Tested a few files by moving them to an app folder but moved them back. May change the file structure to optimize after project is completed. Move them back for now.

--- birth_records/admin.py
+++ birth_records/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import NaturalizationRecord
+from .models import BirthRecord
 
 # Register your models here.
 # ------------------------------------------
 
 # Register the NaturalizationRecord model with the admin site.
 # ----------------------------------------
-admin.site.register(NaturalizationRecord)
+admin.site.register(BirthRecord)
