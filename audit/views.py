7acfbe977cbd392592e47d7369a1fac25708129e
Veries Seals <veries@me.com>
2026-04-11 12:55:29 -0500

    In audit views.py created class AuditLogViewSet, defined queryset and serializer_class

--- audit/views.py
+++ audit/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import AuditLog
+from .models import AuditLogSerializer
 
 # Create your views here.
+# ---------------------------------------------------------------
+
+# Using ModelViewSet to automatically provide list, create, retrieve, update, and destroy actions.
+# This allows us to easily manage our AuditLog objects through the API without having to write additional code for each action.
+# ---------------------------------------------------------------
+class AuditLogViewSet(viewsets.ReadOnlyModelViewSet):
+    # The queryset defines the set of data that will be used for the viewset. In this case, we are retrieving all AuditLog objects from the database.
+    # ---------------------------------------------------------------
+    queryset = AuditLog.objects.all()
+    # The serializer_class specifies the serializer that will be used to convert the AuditLog objects to and from JSON format. This allows us to easily serialize and deserialize our data when interacting with the API.
+    # ---------------------------------------------------------------
+    serializer_class = AuditLogSerializer
+
